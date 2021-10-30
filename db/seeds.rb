d699a2d76548ee53f12622f733f22911badd7c30
jeff <jmorton138@gmail.com>
2021-10-30 10:24:52 +0100

    Populated seeds with four airports

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
+sfo = Airport.create(airport_code: "SFO")
+lax = Airport.create(airport_code: "LAX")
+lho = Airport.create(airport_code: "LHO")
+lgw = Airport.create(airport_code: "LGW")
+
+
+
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +16,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
