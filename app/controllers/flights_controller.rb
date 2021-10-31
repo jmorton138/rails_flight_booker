56f3eacf5cd10ce6db8433a11b63d3a5dc036ea5
jeff <jmorton138@gmail.com>
2021-10-31 12:31:07 +0000

    Add search form with select dropdowns

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,14 @@
 class FlightsController < ApplicationController
+
+    def index
+        @flight = Flight.new
+        @airport_options = Airport.all.map { |a| [a.airport_code, a.id] }
+        @date_options = Flight.all.map { |d| [d.departure_time_formatted, d.id] }
+        @passenger_options = 4.times.map { |i| [i + 1, i + 1] }
+    #     @departure_airport_options = Flight.all.map{ |a| [a.departure_airport.airport_code, a.departure_airport_id] }
+    #     @arrival_airport_options = Flight.all.map{ |a| [a.arrival_airport.airport_code, a.arrival_airport_id] }
+    end
+
+    def search
+    end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+
+  def departure_time_formatted
+    departure_time.strftime("%m/%d/%Y")
+  end
 end
