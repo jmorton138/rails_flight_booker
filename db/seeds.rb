dd3291a36d50f1f62fa618026724879269ba59bd
jeff <jmorton138@gmail.com>
2021-10-31 09:43:08 +0000

    Seed file populated with flights associated with airports

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,27 @@
-sfo = Airport.create(airport_code: "SFO")
-lax = Airport.create(airport_code: "LAX")
-lho = Airport.create(airport_code: "LHO")
-lgw = Airport.create(airport_code: "LGW")
+sfo = Airport.new(airport_code: "SFO")
+sfo.save
+lax = Airport.new(airport_code: "LAX")
+lax.save
+lho = Airport.new(airport_code: "LHO")
+lho.save
+lgw = Airport.new(airport_code: "LGW")
+lho.save
 
 
 
 
-
-
-
+f1 = Flight.new(departure_time: '2021-12-20 12:05:00', duration: 7200)
+f1.departure_airport = sfo
+f1.arrival_airport = lax
+f1.save
+f2 = Flight.new(departure_time: '2021-12-20 14:05:00', duration: 35200)
+f2.departure_airport = sfo
+f2.arrival_airport = lho
+f2.save
+f3 = Flight.new(departure_time: '2021-12-20 16:05:00', duration: 35200)
+f3.departure_airport = lax
+f3.arrival_airport = lgw
+f3.save
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -17,4 +30,5 @@ lgw = Airport.create(airport_code: "LGW")
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# YYYY-MM-DD HH:MM:SS
 
