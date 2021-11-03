9e97dc3618de23f084b43959c07a4dabe778d847
jeff <jmorton138@gmail.com>
2021-11-03 09:44:45 +0000

    Booking form accepts properly hashed params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,11 +3,26 @@ class BookingsController < ApplicationController
     def new
         @flight = Flight.where(id: params[:flight_id])[0]
 
-        @params = params[:num_tickets]
+        @num_tickets = params[:num_tickets]
         @booking = Booking.new
+        @passengers = @num_tickets.to_i.times.map do |p|
+            @booking.passengers.build
+        end
+        @booking
     end
 
     def create
+       # @flight = Flight.where(id: params[:flight_id])[0]
+       # @booking = Booking.new(passenger_params)
+        # if @booking.save?
+        #     @flight.bookings << @booking
+        #     redirect_to booking_path
+        # else
+        #     flash[:error] ="error"
+        #     render :new
+        # end
+
+        
     end
 
     def show
@@ -16,6 +31,6 @@ class BookingsController < ApplicationController
     private
 
     def passenger_params
-        params.require(:passenger).permit(:name, :email)
+        params.require(:booking).permit(:flight_id, :passenger => [ :name, :email ])
     end
 end
