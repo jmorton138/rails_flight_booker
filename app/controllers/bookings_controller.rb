f799febf6cefaf1ac349463343e5488b2e4e0e18
jeff <jmorton138@gmail.com>
2021-11-07 12:32:42 +0000

    thank_you mailer method sends emails to every passenger

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -15,11 +15,16 @@ class BookingsController < ApplicationController
        @flight = Flight.where(id: params[:flight_id])[0]
        @booking = Booking.new(booking_params)
        @flight.bookings << @booking
-        if @booking.save
-            redirect_to booking_path(@booking.id)
-        else
-            flash.alert ="error"
-            render :new
+       respond_to do |format|
+            if @booking.save
+                @passengers = @booking.passengers.each do |passenger|
+                    PassengerMailer.with(passenger: passenger).thank_you_email.deliver_now
+                end
+                format.html { redirect_to booking_path(@booking.id) }
+            else
+                flash.alert ="error"
+                render :new
+            end
         end
 
         
--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -2,7 +2,7 @@ class PassengerMailer < ApplicationMailer
     default from: 'notifications@example.com'
 
     def thank_you_email
-        @passenger = params[:name]
+        @passenger = params[:passenger]
         @url = 'http://example.com/'
         mail(to: @passenger.email, subject: 'Thank you!')
     end
