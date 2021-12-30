5a6d4a45a30f752539fcc4cc04bc4b890945a58a
jeff <jmorton138@gmail.com>
2021-12-30 10:58:36 +0000

    Minor edit to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,25 +18,25 @@ den.save
 
 
 
-# f1 = Flight.new(departure_time: '2021-12-20 12:05:00', duration: 7200)
-# f1.departure_airport = sfo
-# f1.arrival_airport = lax
-# f1.save
+f1 = Flight.new(departure_time: '2021-12-20 12:05:00', duration: 7200)
+f1.departure_airport = sfo
+f1.arrival_airport = lax
+f1.save
 
-# f2 = Flight.new(departure_time: '2021-12-21 14:05:00', duration: 35200)
-# f2.departure_airport = sfo
-# f2.arrival_airport = lho
-# f2.save
+f2 = Flight.new(departure_time: '2021-12-21 14:05:00', duration: 35200)
+f2.departure_airport = sfo
+f2.arrival_airport = lho
+f2.save
 
-# f3 = Flight.new(departure_time: '2021-12-22 16:05:00', duration: 35200)
-# f3.departure_airport = lax
-# f3.arrival_airport = lgw
-# f3.save
+f3 = Flight.new(departure_time: '2021-12-22 16:05:00', duration: 35200)
+f3.departure_airport = lax
+f3.arrival_airport = lgw
+f3.save
 
-# f4 = Flight.new(departure_time: '2021-12-20 10:05:00', duration: 7200)
-# f4.departure_airport = sfo
-# f4.arrival_airport = lax
-# f4.save
+f4 = Flight.new(departure_time: '2021-12-20 10:05:00', duration: 7200)
+f4.departure_airport = sfo
+f4.arrival_airport = lax
+f4.save
 Airport.all.each do |a1|
     Airport.all.each do |a2|
         flight = Flight.new(departure_time: '2021-12-21 14:05:00', duration: rand(7200...100000))
